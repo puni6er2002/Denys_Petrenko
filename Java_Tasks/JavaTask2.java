bf98d30b17e6ddf2cdc35582284bda7ad90e2da6
Denis Petrenko <94494142+puni6er2002@users.noreply.github.com>
2021-12-22 02:38:07 +0200

    Update JavaTask2.java

--- Java_Tasks/JavaTask2.java
+++ Java_Tasks/JavaTask2.java
@@ -1,1 +1,8 @@
-
+public static String FirstNonRepeatingLetter(String str) {
+        var lowercase = str.toLowerCase();
+        for(int i = 0; i < lowercase.length(); i++) {
+            if(lowercase.indexOf(lowercase.charAt(i), lowercase.indexOf(lowercase.charAt(i))+1) == -1)
+                return String.valueOf(str.charAt(i));
+        }
+        return "";
+    }
