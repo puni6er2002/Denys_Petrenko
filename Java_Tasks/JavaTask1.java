75fa81ba54bce3ea8cae7946e9cb599bab91a530
Denis Petrenko <94494142+puni6er2002@users.noreply.github.com>
2021-12-22 02:32:24 +0200

    Update JavaTask1.java

--- Java_Tasks/JavaTask1.java
+++ Java_Tasks/JavaTask1.java
@@ -1,1 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Task1 {
+    public static List<Integer> getIntegersFromList(List<Object> Itemlist) {
+      ArrayList<Integer> res = new ArrayList<Integer>();
+        for(Object element: Itemlist){
+            if(element instanceof Integer){
+                res.add((Integer) element);
+            }
+        }
+        return res;
+    }
